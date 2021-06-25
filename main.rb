baabe9327bc8fe60c68b0ab1c4d14debca7afac3
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2021-06-25 11:41:32 +0200

    add default arg

--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ config = YAML.load_file('./config.yml')
 port = ARGV[0] || 25
 mailAdress = ARGV[1] || "mail.cpnv.ch"
 from = ARGV[2] || config.dig("from")
-message = ARGV.shift
+message = ARGV.shift || "This is a mail test !"
 
 mail = Mail.new(from)
 
