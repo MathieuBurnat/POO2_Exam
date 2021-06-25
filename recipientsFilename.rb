5fa3e7c9270d401b06ea07f1a6e0323368163e75
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2021-06-25 10:59:42 +0200

    load the configuration

--- recipientsFilename.rb
+++ recipientsFilename.rb
@@ -2,6 +2,7 @@ require "./config"
 
 class RecipientsFilename
     def addConfTo(recipients)
+        config = YAML.load_file('./config.yml')
         return recipients.concat(File.readlines(config.dig("recipients_filename"), chomp: true))
     end 
 end
