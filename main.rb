c63391ae3757c47028ad77e705063dd7ea581ab7
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2021-06-25 11:06:18 +0200

    add more recipients

--- main.rb
+++ main.rb
@@ -7,12 +7,12 @@ config = YAML.load_file('./config.yml')
 port = ARGV[0] || 25
 mailAdress = ARGV[1] || "mail.cpnv.ch"
 from = ARGV[2] || config.dig("from")
-
-
 message = ARGV.shift
 
 mail = Mail.new(from, recipients)
 mail.addRecipient("mathieu.burnat@cpnv.ch")
+mail.addRecipient("pascal.hurni.nospam@cpnv.ch")
+mail.addRecipient("alexandre.philbert.nospam@cpnv.ch")
 
 mail.create(message)
 
